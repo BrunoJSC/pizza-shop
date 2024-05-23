6ea8f00990bee8ce982994500f617349187474f5
Bruno de Jesus <bruno.jesus.carmo@gmail.com>
2024-05-23 12:50:24 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { RouterProvider } from 'react-router-dom'
+
+import { Toaster } from './components/ui/sonner'
+import { router } from './routes'
+
+export function App() {
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={router} />
+
+      <Toaster richColors />
+    </HelmetProvider>
+  )
+}
